3b9ac9aec6416b4d3c60edee541706c5eecb4564
MEGA <MEGA@gmail.com>
2025-03-07 14:50:39 +0200

    Add project files.

---
+++ ConsoleApp7/DoublyLinkedList.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp7
+{
+    internal class DoublyLinkedList
+    {
+        public class Node
+    { 
+        public int Data;
+        public Node Next;
+        public Node Prev;
+
+        public Node(int data)
+        {
+            Data = data;
+            Next = null;
+            Prev = null;
+        }
+    }
+
+    public Node Head;
+        public void Reverse()
+        {
+            Node prevNode = null;
+            Node currNode = Head;
+            Node nextNode = null;
+
+            while (currNode != null)
+            {
+                nextNode = currNode.Next;
+                currNode.Next = prevNode;
+                currNode.Prev = nextNode;
+
+                prevNode = currNode;
+                currNode = nextNode; 
+            }
+
+            Head = prevNode;
+        }
+        public void AddFirst(int data)
+        {
+            Node newNode = new Node(data);
+            if (Head != null)
+            {
+                newNode.Next = Head;
+                Head.Prev = newNode;
+            }
+            Head = newNode;
+        }
+
+        public void PrintList()
+        {
+            Node current = Head;
+            while (current != null)
+            {
+                Console.Write(current.Data + " ");
+                current = current.Next;
+            }
+            Console.WriteLine();
+        }
+    }
+
+}
+
---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DoublyLinkedList list = new DoublyLinkedList();
+            list.AddFirst(1);
+            list.AddFirst(2);
+            list.AddFirst(3);
+            list.AddFirst(4);
+            list.AddFirst(5);
+
+            Console.WriteLine("Оригинален списък:");
+            list.PrintList();
+
+            list.Reverse();
+
+            Console.WriteLine("Обърнат списък:");
+            list.PrintList();
+        }
+    }
+}
